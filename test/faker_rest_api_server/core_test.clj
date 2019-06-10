0d991a716249a0890c1579de93a2eeee09f93d69
Abhinav Sharma <abhi18av@outlook.com>
2019-06-10 13:53:58 +0530

    initial commit

---
+++ test/faker_rest_api_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns faker-rest-api-server.core-test
+  (:require [clojure.test :refer :all]
+            [faker-rest-api-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
